a9077de07a0deab2cbc33cec05ea249c3450048e
purushottamdeshmukh <deshmukh259.2010@gmail.com>
2024-03-20 08:53:27 +0530

    c

---
+++ src/main/java/com/sec/springsec1/SpringSec1Application.java
@@ -0,0 +1,13 @@
+package com.sec.springsec1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSec1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSec1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/sec/springsec1/SpringSec1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sec.springsec1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSec1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
